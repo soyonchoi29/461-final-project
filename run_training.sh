bd4927743dde586954099a57ec6a62ea66803ce1
Soyon <sochoi25@amherst.edu>
2024-05-12 22:19:48 -0400

    minor error

--- run_training.sh
+++ run_training.sh
@@ -1,7 +1,8 @@
-LOG_DIR=./logs/alex_train_logs
+LOG_DIR=./logs/alexnet_train_logs
 
 mkdir ./models
+mkdir ${LOG_DIR}
 for num_blocks in 1 2 4 8
 do
-  python3 block_alexnet.py -b ${num_blocks} | tee ./logs/alex_train_logs/${num_blocks}_blocks.txt || true
+  (python3 block_alexnet.py -b ${num_blocks} | tee ${LOG_DIR}/${num_blocks}_blocks.txt) || true
 done
