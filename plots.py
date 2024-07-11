557252d44cd3705151823674d8dc249ee544b153
Soyon <sochoi25@amherst.edu>
2024-07-11 08:00:05 -0400

    plots

--- plots.py
+++ plots.py
@@ -2,4 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# read csv file into pandas dataframe
+df = pd.read_csv('simulations - mNPUSim.csv')
 
+alexnet = df.loc[df[''] == 3, 'A']
+resnet50 =
+gpt2 =
+deepspeech2 =
