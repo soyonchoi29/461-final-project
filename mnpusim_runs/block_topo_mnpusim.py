1af941c7472a9d9345bfea04572be6b74e2501d8
Soyon <sochoi25@amherst.edu>
2024-07-28 19:28:25 -0400

    naming issues

--- mnpusim_runs/block_topo_mnpusim.py
+++ mnpusim_runs/block_topo_mnpusim.py
@@ -146,11 +146,19 @@ if __name__ == '__main__':
                         default='alexnet.csv',
                         help="Name of network topology file to block"
                         )
-
+    parser.add_argument('-v', metavar='Version choice: Depth or Width?', type=str,
+                        default='d',
+                        help='Choice of blocking method, w for width and d for depth')
 
     args = parser.parse_args()
     num_blocks = args.b
     topo_file = args.t
+    vers = args.v
 
     topology = Topo()
-    topology.block_topo_depth(topo_file, num_blocks, first_run=False)
+    if vers == 'd':
+        topology.block_topo_depth(topo_file, num_blocks, first_run=False)
+    elif vers == 'w':
+        topology.block_topo_width(topo_file, num_blocks, first_run=False)
+    else:
+        print('Error: Blocking method not selected.')
--- mnpusim_runs/run_blocks.sh
+++ mnpusim_runs/run_blocks.sh
@@ -1,17 +1,21 @@
 RES_DIR=./results/
 
-# run this script with the single parameter of the name of the run
+# run this script with the command line arguments below:
+# 1. network name (e.g. alexnet, resnet50)
+# 2. number of blocks
+# 3. blocking mode ('w' for width or 'd' for depth)
+
 
 python3 block_topo_mnpusim.py -b ${2} -t "${1}.csv"
 scp ./Makefile ./mNPUsim/Makefile
-mkdir "./results/${1}_${2}blocks"
+mkdir "./results/${1}_${2}${3}blocks"
 
 for i in $(seq 0 $((${2}-1)))
 do
     cd mNPUsim
-    NAME="${1}_${2}o${i}_block"
+    NAME="${1}_${2}o${i}_${3}block"
     make "${NAME}"
     cd ..
-    scp -r "./mNPUsim/${NAME}" "./results/${1}_${2}blocks/${NAME}"
+    scp -r "./mNPUsim/${NAME}" "./results/${1}_${2}${3}blocks/${NAME}"
     echo "block ${i} done!"
 done
